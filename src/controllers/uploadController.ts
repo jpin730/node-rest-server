e60384f7d6371277dbb8b15f4955e96b4b148d01
Jaime Pineda <jpin730@pm.me>
2023-08-30 17:52:55 -0600

    feat: :sparkles: update user avatar using cloudinary

--- src/controllers/uploadController.ts
+++ src/controllers/uploadController.ts
@@ -1,6 +1,40 @@
 import { RequestHandler } from 'express';
+import { FileArray, UploadedFile } from 'express-fileupload';
+import { Document, Types } from 'mongoose';
+import { v2 as cloudinary } from 'cloudinary';
 
-export const uploadFile: RequestHandler = (req, res) => {
-  const { body } = req;
-  res.status(201).json({ body });
+import { IUser, User } from '../models/user';
+
+type UserDocument = Document<unknown, unknown, IUser> &
+  IUser & {
+    _id: Types.ObjectId;
+  };
+
+export const putAvatar: RequestHandler = async (req, res) => {
+  const { file } = req.files as FileArray;
+  const { id } = req.params;
+
+  try {
+    const updatedUser = (await User.findById(id)) as UserDocument;
+
+    const folder = 'node-rest-server';
+
+    if (updatedUser.avatar) {
+      const regex = new RegExp(`.+(${folder}.+)\\..*`);
+      const public_id = updatedUser?.avatar.replace(regex, '$1');
+      await cloudinary.uploader.destroy(public_id as string);
+    }
+
+    const { tempFilePath } = file as UploadedFile;
+
+    const { secure_url } = await cloudinary.uploader.upload(tempFilePath, {
+      folder,
+    });
+    updatedUser.avatar = secure_url;
+    await updatedUser.save();
+
+    res.status(201).json({ updatedUser });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
--- src/routes/uploadRouter.ts
+++ src/routes/uploadRouter.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
 
-import { uploadFile } from '../controllers/uploadController';
+import { putAvatar } from '../controllers/uploadController';
+import { check } from 'express-validator';
+import { validateFields } from '../middlewares/validateFields';
+import { userExist } from '../helpers/validators';
+import { validateFile } from '../middlewares/validateFile';
 
 const uploadRouter = Router();
 
-uploadRouter.post('/', uploadFile);
+uploadRouter.put(
+  '/avatar/:id',
+  [
+    validateFile,
+    check('id').isMongoId(),
+    check('id').custom(userExist),
+    validateFields,
+  ],
+  putAvatar,
+);
 
 export { uploadRouter };
